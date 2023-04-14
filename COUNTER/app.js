efafc648f8136cd21989df3c732086f2709a0ef5
Mizgin Yıldırak <101987484+MizginYildirak@users.noreply.github.com>
2023-04-15 01:44:12 +0300

    Update app.js

--- COUNTER/app.js
+++ COUNTER/app.js
@@ -1,22 +1,33 @@
-let increase = document.getElementById('btn-increase');
-let reset = document.getElementById('btn-reset');
-let decrease = document.getElementById('btn-decrease');
+let increase = document.getElementById("btn-increase");
+let reset = document.getElementById("btn-reset");
+let decrease = document.getElementById("btn-decrease");
 let output = document.querySelector("#output");
 
-increase.addEventListener("click", function() {
-    let result = Number(output.innerText) + 1;
+function handleColorChange(counter) {
+  if (counter > 0) {
+    output.style.color = "green";
+  } else if (counter < 0) {
+    output.style.color = "red";
+  } else {
+    output.style.color = "black";
+  }
+}
 
-    output.innerText = result;
-}) 
+increase.addEventListener("click", function () {
+  let result = Number(output.innerText) + 1;
 
+  output.innerText = result;
+  handleColorChange(result);
+});
 
-decrease.addEventListener("click", function() {
-    let result = Number(output.innerText) - 1;
+decrease.addEventListener("click", function () {
+  let result = Number(output.innerText) - 1;
 
-    output.innerText = result;
-}) 
+  output.innerText = result;
+  handleColorChange(result);
+});
 
-
-reset.addEventListener("click", function() {
-    output.innerText = 0;
-})
+reset.addEventListener("click", function () {
+  output.innerText = 0;
+  handleColorChange(0);
+});
